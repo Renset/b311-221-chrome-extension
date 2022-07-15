c64e48c5045cb9422af7051f6ee5c2aaa1f67993
Renat Notfullin <renat@notfullin.com>
2022-07-16 00:01:23 +0300

    🛠 Fix hearbeat URL

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
-chrome.alarms.create({ delayInMinutes: 1 });
+chrome.alarms.create({ periodInMinutes: 1 });
 
 
 chrome.alarms.onAlarm.addListener(() => {
   heartbeatCall()
   
   function heartbeatCall() {
-    fetch("http://192.168.8.1/api/")
+    fetch("http://192.168.8.1/api/user/heartbeat")
 }
 });
 
